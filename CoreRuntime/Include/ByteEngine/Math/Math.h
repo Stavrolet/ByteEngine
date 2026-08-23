c076a5a28e7c1f632657b1026eff5286c97dc688
Stavrolet <vladneh21@gmail.com>
2026-08-23 22:55:34 +0300

    Add `Mathf` methods `RoundTo`, `CeilTo`, `FloorTo`

--- CoreRuntime/Include/ByteEngine/Math/Math.h
+++ CoreRuntime/Include/ByteEngine/Math/Math.h
@@ -433,11 +433,20 @@ namespace ByteEngine::Math::Mathf
     template <std::floating_point T>
     [[nodiscard]] inline T Round(T value) noexcept { return std::round(value); }
 
+    template<std::floating_point T, Arithmetic U>
+    [[nodiscard]] inline U RoundTo(T value) noexcept { return static_cast<U>(std::round(value)); }
+
     template <std::floating_point T>
     [[nodiscard]] inline T Ceil(T value) noexcept { return std::ceil(value); }
 
+    template<std::floating_point T, Arithmetic U>
+    [[nodiscard]] inline U CeilTo(T value) noexcept { return static_cast<U>(std::ceil(value)); }
+
     template <std::floating_point T>
     [[nodiscard]] inline T Floor(T value) noexcept { return std::floor(value); }
+
+    template<std::floating_point T, Arithmetic U>
+    [[nodiscard]] inline U FloorTo(T value) noexcept { return static_cast<U>(std::floor(value)); }
 
     template <Arithmetic T>
     [[nodiscard]] constexpr T Sign(T value) noexcept { return static_cast<T>((value > 0) - (value < 0)); }
